d8d22741f287777c102c55828cbddba7ebd8c115
Okarm01 <117302512+Okarm01@users.noreply.github.com>
2023-08-30 15:36:23 +0200

    online.js

--- online.js
+++ online.js
@@ -47,6 +47,12 @@ function registerData() {
         eraseContent()
         alert('User Created')
     })
+    .catch(function(error) {
+        if (error.code === ('auth/email-already-in-use') ) {
+            mailAlreadyUsed.style.display = 'block'
+            emailRegister.style.border = '1px solid red'
+        }
+    })
      
 }
   
@@ -54,7 +60,9 @@ function registerData() {
 function loginData(){
     email = emailLogin.value
     password = passwoardLogin.value
-    
+    toManyRequests.style.display = 'none'
+    incorrectEmail.style.display = 'none'
+    incorrectPassword.style.display = 'none'
 
     auth.signInWithEmailAndPassword(email, password)
     .then(function(){
@@ -73,14 +81,25 @@ function loginData(){
         }
       
         database_ref.child('users/' + user.uid).update(user_data)
-        incorectData.style.display = 'none'
+        
+        
         alert('User Logged In ')
         eraseContent()
         
     })
     .catch(function(error) {
-        if (error.code === ('auth/user-not-found') || ('400')) {
-            incorectData.style.display = 'block'
+
+        console.log(error.code)
+        if (error.code === ('auth/user-not-found')) {
+            incorrectEmail.style.display = 'block'
+            emailLogin.style.border = '1px solid red'
+        }
+        if (error.code === ('auth/wrong-password')) {
+            incorrectPassword.style.display = 'block'
+            passwoardLogin.style.border = '1px solid red'
+        }
+        if (error.code === ('auth/too-many-requests')) {
+            toManyRequests.style.display = 'block'
         }
     })
    
